3c93ff8836f6fa85b995c1f3c6ed8ceabdeb362e
Jay Vaghela <20it157@charusat.edu.in>
2022-09-12 14:00:58 +0530

    StudentList

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,19 +1,33 @@
 import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Container, Table } from "react-bootstrap";
+import axios from "axios";
 
 export default function StudentList() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  let getStudents = () => {
+    axios
+      .get("http://localhost:8080/listStudents")
+      .then((response) => setStudents(response.data))
+      .catch((error) => alert(error));
+  };
   return (
     <div className="my-3">
       <Container>
         <Card.Header><h3>Students List</h3></Card.Header>
+        <Card.Header>
+          <h3>Students List</h3>
+        </Card.Header>
         <Card.Body>
           <Table striped bordered hover>
             <thead>
-              <tr>
-                <th>Student Id</th>
-                <th>Student Name</th>
-                <th>Student Address</th>
-              </tr>
+export default function StudentList() {
+            </tr>
             </thead>
             <tbody>
               <tr>
@@ -30,6 +44,19 @@ export default function StudentList() {
                 <td>3</td>
                 <td colSpan={2}>Larry the Bird</td>
               </tr>
+              {students.length === 0 ? (
+                <tr>
+                  <td colSpan={3}>{students.length} Studnets Available!!!</td>
+                </tr>
+              ) : (
+                students.map((student)=>
+                <tr>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.address}</td>
+                </tr>
+                )
+              )}
             </tbody>
           </Table>
         </Card.Body>
