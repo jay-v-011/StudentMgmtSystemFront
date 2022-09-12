04941011995b3af032edf77bd0ac130c78b786cc
Jay Vaghela <20it157@charusat.edu.in>
2022-09-12 13:56:37 +0530

    Added NavigationBar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <div className="App">
       <Student/>
       <StudentList/>
+      <NavigationBar/>
     </div>
   );
 }
